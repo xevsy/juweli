3c481b1ee01d5f68bc30102c0d363d4636b81799
Eugeny Syrotiuk <esyr@efisco.net>
2018-10-14 17:02:40 +0200

    Fix heroku listener

--- server/server.js
+++ server/server.js
@@ -10,6 +10,6 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(publicPath, 'index.html'));
 });
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
   console.log('Server is up!');
 });
