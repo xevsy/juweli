032722450cfeec2011180ca12a185be7e7a53a31
Eugeny Syrotiuk <esyr@efisco.net>
2019-06-12 23:42:05 +0200

    Send email integration

--- server/server.js
+++ server/server.js
@@ -4,7 +4,28 @@ const app = express();
 const publicPath = path.join(__dirname, '..', 'build');
 const port = process.env.PORT || 3000;
 
+const { email, set_template_directory } = require('sendemail')
+set_template_directory('./templates')
+
 app.use(express.static(publicPath));
+
+app.post("/api/send_email", function(req, res) {
+  res.set("Content-Type", "application/json");
+
+  var person = {
+    name : "Jenny",
+    email: "ievgen.syrotiuk@gmail.com",
+    subject:"Welcome to DWYL :)"
+  }
+  /* Send email here */
+  email('order', person, function(error, result){
+    console.log(' - - - - - - - - - - - - - - - - - - - - -> email sent: ');
+    console.log(result);
+    console.log(' - - - - - - - - - - - - - - - - - - - - - - - - - - - -')
+  })
+
+  res.send('{"message":"Email sent."}');
+});
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(publicPath, 'index.html'));
--- src/components/forms/ApplyOrderForm.js
+++ src/components/forms/ApplyOrderForm.js
@@ -8,25 +8,50 @@ import DialogActions from '@material-ui/core/DialogActions/DialogActions'
 import Favorite from "@material-ui/icons/Favorite"
 import Button from '@material-ui/core/Button/Button'
 import Dialog from '@material-ui/core/Dialog/Dialog'
+import { TextValidator, ValidatorForm } from 'react-material-ui-form-validator'
+import classNames from 'classnames'
+import InputAdornment from '@material-ui/core/InputAdornment/InputAdornment'
+import AccountCircle from '@material-ui/icons/AccountCircle'
+import Phone from '@material-ui/icons/Phone'
+import Email from '@material-ui/icons/Email'
+import axios from 'axios'
 
 class ApplyOrderForm extends React.PureComponent {
   constructor(props) {
     super(props);
     this.state = {
-      open: false
+      open: false,
+      fio: '',
+      email: '',
+      phone: '',
+      comments: '',
     }
   }
 
-  handleChange = (value) => {
-
-  }
+  handleChange = prop => event => {
+    this.setState({ [prop]: event.target.value });
+  };
 
   handleCloseDialog = () => {
     this.setState({ open: false });
   }
 
-  handleApplyOrder = () => {
+  handleApplyOrder = (e) => {
+    e.preventDefault();
+    console.log(this.state)
+    console.log(this.props)
 
+    const data = {
+      message: 'Test message'
+    }
+
+    axios.post('/api/send_email', data)
+      .then( res => {
+        console.log(res)
+      })
+      .catch( () => {
+        console.log('Message not sent')
+      })
   }
 
   applyOrder = () => {
@@ -47,17 +72,89 @@ class ApplyOrderForm extends React.PureComponent {
           onClose={this.handleCloseDialog}
           aria-labelledby="form-dialog-title"
         >
-          <DialogTitle id="form-dialog-apply-order">{T.translate("common.applyOrderTitle")}</DialogTitle>
-          <DialogContent>
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={this.handleCloseDialog} color="inherit">
-              {T.translate("common.cancelButton")}
-            </Button>
-            <Button onClick={this.handleApplyOrder} color="primary">
-              {T.translate("common.applyOrderButton")}
-            </Button>
-          </DialogActions>
+          <ValidatorForm
+            ref="form"
+            onSubmit={(event) => {this.handleApplyOrder(event)}}
+            onError={errors => console.log(errors)}
+          >
+            <DialogTitle id="form-dialog-apply-order">{T.translate("common.applyOrderTitle")}</DialogTitle>
+            <DialogContent>
+                <TextValidator
+                  id={"fio"}
+                  label={T.translate("common.fio")}
+                  name={"fio"}
+                  variant={"outlined"}
+                  className={classNames(classes.margin, classes.textField)}
+                  value={this.state.fio}
+                  onChange={this.handleChange('fio')}
+                  validators={['required']}
+                  errorMessages={[T.translate("cabinet.requiredField")]}
+                  margin={"normal"}
+                  InputProps={{
+                    startAdornment: (
+                      <InputAdornment position="start">
+                        <AccountCircle />
+                      </InputAdornment>
+                    ),
+                  }}
+                />
+                <TextValidator
+                  id={"email"}
+                  label={T.translate("common.email")}
+                  name={"email"}
+                  variant={"outlined"}
+                  className={classNames(classes.margin, classes.textField)}
+                  value={this.state.email}
+                  onChange={this.handleChange('email')}
+                  margin={"normal"}
+                  InputProps={{
+                    startAdornment: (
+                      <InputAdornment position="start">
+                        <Email />
+                      </InputAdornment>
+                    ),
+                  }}
+                />
+                <TextValidator
+                  id={"phone"}
+                  label={T.translate("common.phone")}
+                  name={"phone"}
+                  variant={"outlined"}
+                  className={classNames(classes.margin, classes.textField)}
+                  value={this.state.phone}
+                  onChange={this.handleChange('phone')}
+                  validators={['required']}
+                  errorMessages={[T.translate("cabinet.requiredField")]}
+                  margin={"normal"}
+                  InputProps={{
+                    startAdornment: (
+                      <InputAdornment position="start">
+                        <Phone />
+                      </InputAdornment>
+                    ),
+                  }}
+                />
+                <TextValidator
+                  id={"comments"}
+                  label={T.translate("common.comments")}
+                  name={"comments"}
+                  multiline
+                  variant={"outlined"}
+                  className={classNames(classes.margin, classes.textField)}
+                  value={this.state.comments}
+                  onChange={this.handleChange('comments')}
+                  margin={"normal"}
+                />
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={this.handleCloseDialog} color="inherit">
+                {T.translate("common.cancelButton")}
+              </Button>
+              <Button type={"submit"} color="primary">
+                {T.translate("common.applyOrderButton")}
+              </Button>
+            </DialogActions>
+          </ValidatorForm>
         </Dialog>
       </div>
     )
